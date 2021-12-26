48492d3926fcf0de749b6d1ef0d778b26731b769
Haram Jeong <niceharam@gmail.com>
2021-12-27 02:17:06 +0900

    fix: background image

--- js/background.js
+++ js/background.js
@@ -2,4 +2,4 @@ const images = ['0.jpg', '1.jpg'];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(/img/${chosenImage})`;
+document.body.style.backgroundImage = `url(./img/${chosenImage})`;
